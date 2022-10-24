0a9ecf4e0194703c3ff9ff418955809f993fd807
naomynaomy <naomyv03@gmail.com>
2022-10-24 00:07:23 -0700

    Completed part 3

--- challenge_folder/loan_analyzer.py
+++ challenge_folder/loan_analyzer.py
@@ -87,6 +87,7 @@ if present_value >= loan["loan_price"]:
 else:
     print("Do not buy! This loan is too expensive.")
 
+print(f"The loan's fair value is:{present_value:.2f}")
 
 
 """Part 3: Perform Financial Calculations.
@@ -111,13 +112,16 @@ new_loan = {
 # @TODO: Define a new function that will be used to calculate present value.
 #    This function should include parameters for `future_value`, `remaining_months`, and the `annual_discount_rate`
 #    The function should return the `present_value` for the loan.
-# YOUR CODE HERE!
-
+def loan_present_value(future_value, remaining_months, annual_discount_rate):
+    present_value = future_value / (1 + (annual_discount_rate/12)) **remaining_months
+    return present_value
 
 # @TODO: Use the function to calculate the present value of the new loan given below.
 #    Use an `annual_discount_rate` of 0.2 for this new loan calculation.
 # YOUR CODE HERE!
-print(f"The present value of the loan is: {present_value}")
+annual_discount_rate = 0.2
+present_value = loan_present_value(new_loan["future_value"], new_loan["remaining_months"], annual_discount_rate)
+print(f"The present value of the loan is: {present_value:.2f}")
 
 
 """Part 4: Conditionally filter lists of loans.
